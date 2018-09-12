72b8917b588992f84dc9bb78bda364bb4d01445f
ava krishn <avakrishn@gmail.com>
2018-09-12 15:58:39 -0700

    require ejs and connect to port

--- server.js
+++ server.js
@@ -4,27 +4,21 @@ require("dotenv").config();
 var fs = require("fs");
 var express = require('express');
 var app = express();
-// var router = express.Router();
+
 var methodOverride = require('method-override');
-// var bcrypt = require('bcryptjs');
-// var bodyParser = require('body-parser');
-// var cookieParser = require('cookie-parser');
-// var session = require('express-session');
+
 var path = require("path");
 
 app.use(methodOverride('_method'));
-// app.use(session({ secret: 'app', cookie: { maxAge: 1*1000*60*60*24*365 }}));
-// app.use(cookieParser());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static content for the app from the "public" directory in the application directory.
-// you need this line here so you don't have to create a route for every single file in the public folder (css, js, image, etc)
-//index.html in the public folder will over ride the root route
+
 app.use(express.static("public"));
 
-//sets EJS available
-// app.set('view engine', 'ejs');
+// sets EJS available
+app.set('view engine', 'ejs');
 
 // Initializes the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
@@ -40,3 +34,6 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,  //placeholder for your own mySQL password that you store in your own .env file
   database: process.env.DB_NAME    //TBD
 });
+
+
+app.listen(3000);
