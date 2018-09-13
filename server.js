000cdda4166a2a879034b1ffbea8415ef760e573
ava krishn <avakrishn@gmail.com>
2018-09-12 21:18:58 -0700

    update server to check if url is valid and remove app from path

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var fs = require("fs");
 var express = require('express');
 var app = express();
 
+var validUrl = require('valid-url');
+
 
 // var router = express.Router();
 var methodOverride = require('method-override');
@@ -23,6 +25,7 @@ app.use(bodyParser.json());
 // Serve static content for the app from the "public" directory in the application directory.
 // you need this line here so you don't have to create a route for every single file in the public folder (css, js, image, etc)
 //index.html in the public folder will over ride the root route
+
 app.use(express.static("public"));
 
 //sets EJS available
@@ -46,24 +49,34 @@ var connection = mysql.createConnection({
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'app/public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 
 app.post('/create_user', function(req, res){
   console.log(req.body);
-  if(req.body.username && req.body.picture_link){
+  // console.log(validUrl.isWebUri(req.body.picture_link));
+
+  // if the user has filled in all fields insert information into database and the picture link is a valid url
+  if(req.body.username && validUrl.isWebUri(req.body.picture_link)){
       
     connection.query('INSERT INTO users (username, picture_link) VALUES (?, ?);', [req.body.username,req.body.picture_link],function(error, results, fields){
       
       if (error) throw error;
-      res.render('pages/roommate_quiz.ejs');
+      res.redirect('/roommate_quiz');
 
     });
   }
+  // if user has left fields blank redirect user back to root
   else{
     res.redirect('/');
   }     
 });
+
+
+app.get('/roommate_quiz', function(req, res) {
+  res.render('pages/roommate_quiz.ejs');
+});
+
 
 app.listen(3000);
