c69ada1fa0e0f0a6e53168adbf6b5e5e5ad5afa0
ava krishn <avakrishn@gmail.com>
2018-09-15 21:11:45 -0700

    when form submits function submitAnswer function checks if an option was selected for each question and then assigns action and method attributes to the form

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -10,3 +10,18 @@ function createUser(){
         alert('Please fill in all of the fields.');
     }
 }
+
+function submitAnswers(){
+    
+    if($("#q_1").val() != null && $("#q_2").val() != null && $("#q_3").val() != null && $("#q_4").val() != null && $("#q_5").val() != null && $("#q_6").val() != null && $("#q_7").val() != null && $("#q_8").val() != null && $("#q_9").val() != null && $("#q_10").val() != null){
+        $('form').attr({
+            action: "/submit_q",
+            method: "POST"
+        });
+    }
+    else{
+        event.preventDefault();
+        alert('Please select an answer for all questions.');
+    }
+    
+}
