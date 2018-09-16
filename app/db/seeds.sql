1b498321f6fe572815996c638d6f8cd33429e54d
ava krishn <avakrishn@gmail.com>
2018-09-15 22:08:14 -0700

    create seeds.sql fileto seed database

---
+++ app/db/seeds.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO users VALUES (1, 'The Hulk','https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/incredible-hulk-marvel-avenger-superhero-pc-background-hd-wallpaper.jpg', '2018-09-08 12:02:30');
+
+INSERT INTO users VALUES (2, 'Iron Man','https://wallpapersite.com/images/pages/ico_n/14496.png', '2018-09-09 14:20:30');
+
+INSERT INTO users VALUES (3, 'Captain America','https://cdn.britannica.com/s:700x450/30/182830-004-61C7794A.jpg', '2018-09-11 15:40:30');
+
+INSERT INTO users VALUES (4, 'Thor','https://static.tvtropes.org/pmwiki/pub/images/mcu_thor.png', '2018-09-13 22:05:30');
+
+
+
+INSERT INTO user_answers VALUES (1, 5, 1, 2, 5, 1, 5, 5, 1, 4, 1);
+
+INSERT INTO user_answers VALUES (2, 4, 5, 4, 1, 4, 5, 4, 5, 3, 3);
+
+INSERT INTO user_answers VALUES (3, 4, 3, 3, 3, 5, 2, 2, 3, 5, 5);
+
+INSERT INTO user_answers VALUES (4, 2, 4, 5, 1, 4, 2, 3, 3, 5, 5);
